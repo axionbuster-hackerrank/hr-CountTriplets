cd0c00308d64b51602c818bb298b70c95238f3b5
YuJin Kim <axionbuster@gmail.com>
2020-01-04 14:53:36 -0800

    Initial Commit

---
+++ count-triplets-001/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  count-triplets-001
+//
+//  Created by YuJin Kim on 1/4/20.
+//  Copyright © 2020 YuJin Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
